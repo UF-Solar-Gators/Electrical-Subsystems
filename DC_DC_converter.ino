58d580e1de052d6e9114f9dbb02fae249f1f1e69
UFsolargators <electrical@ufsolargators.org>
2017-03-22 14:19:49 -0400

    Create DC_DC_converter.ino
    skeleton code

---
+++ DC_DC_converter.ino
@@ -0,0 +1,23 @@
+//DC-DC converter code
+//init vars
+
+//setup code
+void setup() 
+{
+  //initialize ports
+}
+
+//body code
+void loop()
+{
+
+}
+
+//interrupts
+
+
+//functions
+void function()
+{
+
+}
