67ccc3371bbfc7e1cf5c9d17e133ca9b2454b8e0
michellermoore <michellermoore@ufl.edu>
2017-04-04 14:00:13 -0400

    Update DC_DC_converter.ino

--- DC_DC_converter.ino
+++ DC_DC_converter.ino
@@ -1,12 +1,12 @@
 //DC-DC converter code
 //init vars
 
-#ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#endif
-#ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#endif 
+//#ifndef cbi
+//#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+//#endif
+//#ifndef sbi
+//#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+//#endif 
 
 
 //setup code
