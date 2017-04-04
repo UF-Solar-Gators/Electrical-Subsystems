bd34c6f987068097b4dc19a57913968366016e1a
michellermoore <michellermoore@ufl.edu>
2017-04-04 13:56:40 -0400

    Update Motor_Controller.ino

--- Motor_Controller.ino
+++ Motor_Controller.ino
@@ -1,6 +1,13 @@
 //Motor Controller Code
 //init vars
 
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif 
+
 //setup code
 void setup() 
 {
